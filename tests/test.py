6de40216a907dccb7d98376d4106ba8796dec302
Sergei S <div@altertech.com>
2020-03-14 05:24:45 +0100

    tests with roboger.manager

--- tests/test.py
+++ tests/test.py
@@ -11,14 +11,14 @@ from functools import partial
 from types import SimpleNamespace
 from flask import Flask
 
-test_data = SimpleNamespace()
-
 dir_me = Path(__file__).absolute().parents[1]
 os.chdir(dir_me)
 sys.path.insert(0, dir_me.as_posix())
 
 from roboger.server import product_build
 from roboger.server import __version__ as product_version
+
+from roboger.manager import ManagementAPI, Addr
 
 test_server_bind = '127.0.0.1'
 test_server_port = random.randint(9900, 9999)
@@ -27,11 +27,11 @@ test_app_bind = '127.0.0.1'
 test_app_port = random.randint(9800, 9899)
 
 pidfile = '/tmp/roboger-test-{}.pid'.format(os.getpid())
-logfile = '/tmp/roboger-test-gunicorn.log'
-
-headers = {'X-Auth-Key': '123', 'Accept': '*/*'}
+logfile = '/tmp/roboger-test-gunicorn-{}.log'.format(os.getpid())
 
 _test_app = Flask(__name__)
+
+api = ManagementAPI(f'http://{test_server_bind}:{test_server_port}', '123')
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -62,88 +62,56 @@ def start_servers():
         pass
 
 
-@pytest.mark.skip
-def mapi2(uri, method='GET', err=None, timeout=2, **kwargs):
-    callfunc = getattr(requests, method.lower())
-    result = callfunc(
-        f'http://{test_server_bind}:{test_server_port}/manage/v2{uri}',
-        **kwargs,
-        headers=headers,
-        timeout=timeout)
-    if not result.ok:
-        if not err:
-            raise RuntimeError(f'Request status code: {result.status_code}, '
-                               f'text:\n{result.text}')
-        elif err != 'get' and err != result.status_code:
-            raise RuntimeError(
-                f'Request status code: {result.status_code}, expected: {err}, '
-                f'text:\n{result.text}')
-    return result
-
-
 def test001_test_server():
-    result = mapi2('/core').json()
-    assert result['ok'] is True
+    result = api.test()
     assert result['build'] == product_build
     assert result['version'] == product_version
-    result = requests.get(
-        f'http://{test_server_bind}:{test_server_port}/ping')
+    result = requests.get(f'http://{test_server_bind}:{test_server_port}/ping')
     assert result.status_code == 204
 
 
-def test011_create_addr():
-    result = mapi2('/addr', 'POST').json()
-    test_data.addr_id = result['id']
-    test_data.addr = result['a']
-    assert result['active'] == 1
-    int(result['lim'])
-
-
-def test012_get_addr():
-    result = mapi2(f'/addr/{test_data.addr}').json()
-    assert test_data.addr_id == result['id']
-    assert test_data.addr == result['a']
-    assert result['active'] == 1
-    int(result['lim'])
-    result = mapi2(f'/addr/{test_data.addr_id}').json()
-    assert test_data.addr_id == result['id']
-    assert test_data.addr == result['a']
-    assert result['active'] == 1
-    int(result['lim'])
-
-
-def test013_change_addr():
-    result = mapi2(f'/addr/{test_data.addr}', 'POST', json={
-        'cmd': 'change'
-    }).json()
-    assert test_data.addr_id == result['id']
-    assert test_data.addr != result['a']
-    assert result['active'] == 1
-    test_data.addr = result['a']
-    int(result['lim'])
-
-
-def test014_disable_addr():
-    result = mapi2(f'/addr/{test_data.addr}', 'PATCH', json={
-        'active': 0
-    }).json()
-    assert test_data.addr_id == result['id']
-    assert test_data.addr == result['a']
-    assert result['active'] == 0
-
-
-def test015_enable_addr():
-    result = mapi2(f'/addr/{test_data.addr}', 'PATCH', json={
-        'active': 1
-    }).json()
-    assert test_data.addr_id == result['id']
-    assert test_data.addr == result['a']
-    assert result['active'] == 1
-
-
-def test998_delete_addr():
-    assert mapi2(f'/addr/{test_data.addr}', 'DELETE').status_code == 204
-    mapi2(f'/addr/{test_data.addr}', 'DELETE', err=404)
+def test011_addr():
+    # create addr
+    addr = Addr(api=api)
+    addr.create()
+    assert addr.active
+    x = dict(addr)
+    assert x['id']
+    assert x['a']
+    assert x['active']
+    int(addr.lim)
+    # get addr
+    addr2 = Addr(id=addr.id, api=api)
+    addr2.load()
+    assert addr2.id == addr.id
+    assert addr2.a == addr.a
+    assert addr2
+    int(addr.lim)
+    # get addr by id
+    addr2.id = None
+    addr2.load()
+    assert addr2.id == addr.id
+    assert addr2.a == addr.a
+    assert addr2
+    int(addr.lim)
+    # change addr
+    addr.change()
+    assert addr2.id == addr.id
+    assert addr2.a != addr.a
+    assert addr2
+    # disable addr
+    addr.disable()
+    addr2.load()
+    assert not addr.active
+    assert not addr2.active
+    # enable addr
+    addr.enable()
+    assert addr
+    addr2.load()
+    assert addr2
+    addr.delete()
+    with pytest.raises(LookupError, match=r'addr .* not found'):
+        addr2.delete()
 
 
 def test999_cleanup():
