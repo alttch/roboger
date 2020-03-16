370cecf7417cc5cfab936a32e825d07a39c1450e
Sergei S <div@altertech.com>
2020-03-16 18:22:26 +0100

    2.0.2

--- roboger/api.py
+++ roboger/api.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech, http://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2018-2020 Altertech Group'
 __license__ = 'Apache License 2.0'
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 from flask import request, jsonify, Response, abort
 
