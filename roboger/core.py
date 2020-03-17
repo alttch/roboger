9e2c1f718d51dabaa13c379ace710ffd7d02a23f
Sergei S <div@altertech.com>
2020-03-17 18:56:58 +0100

    2.0.17

--- roboger/core.py
+++ roboger/core.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech, http://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2018-2020 Altertech Group'
 __license__ = 'Apache License 2.0'
-__version__ = '2.0.16'
+__version__ = '2.0.17'
 
 import platform
 import os
